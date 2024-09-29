a8b08af6e939de589f1f416eabc6acdecd9a2e90
vishalmeena2211 <meenavishal2211@gmail.com>
2024-09-30 03:43:51 +0530

    fixed origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: 'https://edu-nova-assessment.vercel.app'
 }));
 
 app.use(express.json());
